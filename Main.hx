be18e1c91120f2c96540329ab0ef85bd2e9b8ce6
GiorgioNatili <g.natili@gnstudio.com>
2016-02-04 15:38:01 -0500

    Accessibility sample

--- Main.hx
+++ Main.hx
@@ -15,37 +15,22 @@ class Main {
   static function main() {
     var current = flash.Lib.current;
     // Create sprites
-    var sprite;
-    var tabIndex = 0;
 
-    sprite = makeSprite("Button name 1", 0xFF0000, tabIndex, 0x800a);
-    sprite.y = 10;
-    sprite.x = tabIndex * 110 + 10;
-    current.addChild(sprite);
-    
-    tabIndex++;
-    
-    sprite = makeSprite("Button name 2", 0x00FF00, tabIndex, 0x800a);
-    sprite.y = 10;
-    sprite.x = tabIndex * 110 + 10;
-    current.addChild(sprite);
-    
-    tabIndex++;
+    makeSprite("Button name 1", 0xFF0000, 1, 0x800a);
+    makeSprite("Button name 2", 0x00FF00, 2, 0x800c);
 
-    sprite = makeSprite("Button name 3", 0x0000FF, tabIndex, 0x8005);
-    sprite.y = 10;
-    sprite.x = tabIndex * 110 + 10;
+    var sprite = makeSprite("Button name 3", 0x0000FF, 3, 0x8005);
     sprite.addEventListener(FocusEvent.FOCUS_OUT, function(_) {
         ExternalInterface.call("goAhead");
     });
-
-    current.addChild(sprite);
 
     // Setup accessibility
     current.accessibilityProperties = new AccessibilityProperties();
     current.accessibilityProperties.name = "Testing the accessibility planet";
     current.accessibilityProperties.forceSimple = true;
 
+    Accessibility.updateProperties();
+    
     if (Capabilities.hasAccessibility) {
       Accessibility.updateProperties();
     }
@@ -57,7 +42,7 @@ class Main {
     textField.name = "textField";
     textField.mouseEnabled = false;
 
-    textField.text = "SimpleButton Test";
+    textField.text = name;
 
     var rectangleShape:Shape = new Shape();
     rectangleShape.graphics.beginFill(color);
@@ -75,13 +60,17 @@ class Main {
     simpleButton.downState = simpleButtonSprite;
     simpleButton.hitTestState = simpleButtonSprite;
 
+    simpleButton.y = 10;
+    simpleButton.x = tabIndex * 110 + 10;
+
     simpleButton.addEventListener(FocusEvent.FOCUS_IN, function(_) {
         Accessibility.sendEvent(simpleButton, 0, event);
     });
 
+    flash.Lib.current.addChild(simpleButton);
+
     simpleButton.accessibilityProperties = new AccessibilityProperties();
-    simpleButton.accessibilityProperties.name = "I am accessible";
-    simpleButton.accessibilityProperties.description = "I am accessible" + name;
+    simpleButton.accessibilityProperties.name = "I am really accessible";
 
     return simpleButton;
 
